3567a04a668355d604395b556136610d2fafa214
ichtx <61445711+ichtx@users.noreply.github.com>
2021-06-11 16:05:42 +0800

    sign up page

---
+++ localhost/template/register_handler.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<body>
+		User: <?php echo $_POST["fullname"]; ?><br>
+		Job Position: <?php echo $_POST["position"]; ?><br>
+		Email: <?php echo $_POST["email"]; ?><br>
+		Phone: <?php echo $_POST["phone"]; ?><br>
+		Username: <?php echo $_POST["username"]; ?><br>
+		Password: <?php echo $_POST["password"]; ?><br>
+		Confirm password: <?php echo $_POST["confirmpassword"]; ?><br>
+	</body>
+</html>
