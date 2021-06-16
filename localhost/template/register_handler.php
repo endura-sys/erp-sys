a8e035b83ed08e6a4aedef9b08d46e8002743746
ichtx <61445711+ichtx@users.noreply.github.com>
2021-06-16 12:22:28 +0800

    create user table

--- localhost/template/register_handler.php
+++ localhost/template/register_handler.php
@@ -2,12 +2,23 @@
 <html lang="en">
 	<body>
 		<?php 
+		
+			$fullname = $_POST["fullname"];
+			$position = $_POST["position"];
+			$email = $_POST["email"];
+			$phone = $_POST["phone"];
 			$user = $_POST["username"];
 			$pw = $_POST["password"];
+			$pw2 = $_POST["confirmpassword"];
+			
 			include '../database.php';
 			$conn = OpenCon();
+			$sql = "INSERT INTO user (full_name, position, email, phone, username, password) VALUES('$fullname', '$position', '$email', '$phone', '$user', '$pw')";
+			$result = $conn->query($sql);
+			
 			$sql = "CREATE USER '$user'@'localhost' IDENTIFIED BY '$pw'";
 			$result = $conn->query($sql);
+			
 			/*
 			$sql = "GRANT ALL PRIVILEGES ON *.* TO $user@'localhost'";
 			$result = $conn->query($sql);
