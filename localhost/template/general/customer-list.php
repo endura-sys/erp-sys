596ebd9a37239f253e566ac866122aed9cd37951
watamekawayo <infinite20020130@gmail.com>
2021-06-23 11:40:41 +0800

    add button (can't link to database)

--- localhost/template/general/customer-list.php
+++ localhost/template/general/customer-list.php
@@ -52,6 +52,166 @@
 
                         <div class="card-header">
                             Simple Datatable
+
+                            <button type="button" class="btn btn-outline-primary block float-start float-lg-end" data-bs-toggle="modal"
+                                data-bs-target="#border-add">
+                                Add new data
+                            </button>
+                                <div class="modal fade text-left modal-borderless" id="border-add" tabindex="-1"
+                                    role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Add new data</h5>
+                                                <button type="button" class="close rounded-pill" data-bs-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <i data-feather="x"></i>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form data-target="#border-added" method="post">
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            
+                                                            <div class="form-group">
+                                                                <label for="customer_id">Customer id:</label>
+                                                                <input type="integer" class="form-control" name="customer_id" id="Customer id" placeholder="">
+                                                            </div>
+                                                            
+                                                            
+                                                            <div class="form-group">
+                                                                <label for="name">Customer name:</label>
+                                                                <input type="varchar" class="form-control" name="name" id="Customer name" placeholder="">
+                                                            </div>
+                                                            
+                                                            
+                                                            <div class="form-group">
+                                                                <label for="gender">Gender:</label>
+                                                                <input type="varchar" class="form-control" name="gender" id="Gender" placeholder="">
+                                                            </div>
+
+                                                            <div class="form-group">
+                                                                <label for="contact_no">Contact no:</label>
+                                                                <input type="varchar" class="form-control" name="contact_no" id="Contact no" placeholder="">
+                                                            </div>
+
+                                                        </div>
+                                                        
+                                                        <div class="col-md-6">
+                                                            
+                                                            <div class="form-group">
+                                                                <label for="contact_email">Contact email:</label>
+                                                                <input type="varchar" class="form-control" name="contact_email" id="Contact email" placeholder="">
+                                                            </div>
+                                                            
+                                                            
+                                                            <div class="form-group">
+                                                                <label for="address">Address:</label>
+                                                                <input type="varchar" class="form-control" name="address" id="Address" placeholder="">
+                                                            </div>
+                                                            
+                                                            
+                                                            <div class="form-group">
+                                                                <label for="member_point">Member point:</label>
+                                                                <input type="varchar" class="form-control" name="member_point" id="Member point" placeholder="">
+                                                            </div>
+                                                            
+                                                            <div class="form-group">
+                                                                <label for="date_of_last_purchasing">Date of last purchasing</label>
+                                                                <input type="date" class="form-control" name="date_of_last_purchasing" id="Date of last purchasing" placeholder="">
+                                                            </div>
+                                                    </div>
+                                                    
+                                                    <div class="modal-footer">
+                                                        <form data-target="#border-added" method="post">
+                                                            <input type="submit" class="btn-check" value="Submit" id='submit'>
+                                                            <label class="btn btn-primary" for="submit">Submit</label>
+                                                        </form>
+                                                        <button type="button" class="btn btn-light-primary ml-1" data-bs-dismiss="modal">
+                                                            <i class="bx bx-check d-block d-sm-none"></i>
+                                                            <span class="d-none d-sm-block">Close</span>
+                                                        </button>
+                                                    </div>
+                                                    
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <div class="modal fade text-left modal-borderless" id="border-added" tabindex="-1"
+                                role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Add new data</h5>
+                                            <button type="button" class="close rounded-pill" data-bs-dismiss="modal"
+                                                aria-label="Close">
+                                                <i data-feather="x"></i>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="card-body">
+                                                <tbody>
+                                                    <center>
+                                                        <?php
+                                                            
+                                                            $conn = mysqli_connect("localhost", "root", "root", "db");
+                                                            
+                                                            // Check connection
+                                                            if($conn === false){
+                                                                die("ERROR: Could not connect. " 
+                                                                    . mysqli_connect_error());
+                                                            }
+                                                            
+                                                            $customer_id =  $_REQUEST['customer_id'];
+                                                            $name =  $_REQUEST['name'];
+                                                            $gender = $_REQUEST['gender'];
+                                                            $contact_no = $_REQUEST['contact_no'];
+                                                            $contact_email = $_REQUEST['contact_email'];
+                                                            $address = $_REQUEST['address'];
+                                                            $member_point = $_REQUEST['member_point'];
+                                                            $date_of_last_purchasing = $_REQUEST['date_of_last_purchasing'];
+                                                            
+                                                            // Performing insert query execution
+                                                            $sql = "INSERT INTO customer VALUES ('$customer_id',
+                                                            '$name','$gender','$contact_no','$contact_email',
+                                                            '$address','$member_point','$date_of_last_purchasing')";
+                                                            
+                                                            if(mysqli_query($conn, $sql)){
+                                                                echo "<h3>Data stored in a database successfully." 
+                                                                . " Please browse your localhost" 
+                                                                . " to view the updated data</h3>"; 
+                                                                
+                                                                echo nl2br("Customer id : $customer_id\n"
+                                                                    . "Customer name : $name\nGender : $gender\nContact no : $contact_no\nContact email : $contact_email\nAddress : $address\nMember point : $member_point\nDate of last purchasing : $date_of_last_purchasing\n");
+                                                            } else{
+                                                                // echo "ERROR : Invalid input $sql. "
+                                                                // . mysqli_error($conn);
+                                                                echo "ERROR : Invalid input. " 
+                                                                . mysqli_error($conn);
+                                                            }
+                                                            mysqli_close($conn);
+                                                        ?>
+                                                        <div>
+                                                            <div class="card-body">
+                                                            </div>
+                                                            
+                                                            <input type="addnew" value="Addnew" class="btn-check" id="addnew" 
+                                                                onClick="document.location.href='addnew'" />
+                                                            <label class="btn btn-outline-success" for="addnew">Add another data</label>
+                                                            
+                                                            <input type="mainn" value="Mainn" class="btn-check" id="mainn" 
+                                                                onClick="document.location.href='dashboard'" />
+                                                            <label class="btn btn-outline-danger" for="mainn">Back to database</label>
+                                                        </div>
+                                                        </center>
+                                                </tbody>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
 
                         <div class="card-body">
@@ -76,7 +236,7 @@
                                         include '../../database.php';
                                         $conn = OpenCon();
 
-                                        $sql = "SELECT `customer_id``name``gender``contact_no.``contact_email``address``member_point``date_of_last_purchasing` FROM customer";
+                                        $sql = "SELECT customer_id, name, gender, contact_no, contact_email, address, member_point, date_of_last_purchasing FROM customer";
                                         $result = $conn->query($sql);
 
                                         $product_list = array();
@@ -87,7 +247,7 @@
                                                 // array_push($product_list, array($row["no"], $row["name"], $row["status"], $row["p1"],  $row["p2"],  $row["p3"],  $row["stock"],  $row["location"],  $row["sake_brewer"],  $row["volume"],  $row["unit"] ));
                                                 // echo $product_list[0][2];
                                                 // print_r($product_list);
-                                                echo "<tr><td>" .$row["customer_id"] ."</td><td>" .$row["name"] ."</td><td>" .$row["gender"] ."</td><td>" .$row["contact_no."] ."</td><td>" .$row["contact_email"] . "</td><td>" .$row["address"] ."</td><td>" .$row["member_point"] ."</td><td>" .$row["date_of_last_purchasing"]. "</td>";
+                                                echo "<tr><td>" .$row["customer_id"] ."</td><td>" .$row["name"] ."</td><td>" .$row["gender"] ."</td><td>" .$row["contact_no"] ."</td><td>" .$row["contact_email"] . "</td><td>" .$row["address"] ."</td><td>" .$row["member_point"] ."</td><td>" .$row["date_of_last_purchasing"]."</td><td>". "</td>";
                                             }
                                         } else {
                                             echo "0 results";
