5c5cdf44f484d5e3531af42578730034314ee8c2
litchi <yikelitchi@gmail.com>
2021-07-19 10:34:16 +0800

    composer update

---
+++ localhost/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd1d8a2d6fa295297221e99c65072f219d04c50f5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd1d8a2d6fa295297221e99c65072f219d04c50f5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
