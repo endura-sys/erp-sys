f215ed95256a0db7a0a6a5464968b0d1330ea544
amussali <aitoaaaw@gmail.com>
2021-07-23 12:38:29 +0800

    Fixed Auth, and added some comments

--- template/auth/server.php
+++ template/auth/server.php
@@ -13,50 +13,9 @@ $errors = array();
 // connect to the database
 include '../../database.php';
 $conn = OpenCon();
-
-if (isset($_POST["submitbtn"])) {
-
-
-
-  $fullname = $_POST["fullname"];
-  $position = $_POST["position"];
-  $email = $_POST["email"];
-  $phone = $_POST["phone"];
-  $user = $_POST["username"];
-  $pw = $_POST["password"];
-  $pw2 = $_POST["confirmpassword"];
-
-  if (empty($fullname)) { array_push($errors, "Full Name is required."); }
-  if (empty($position)) { array_push($errors, "Job Position is required."); }
-  if (empty($email)) { array_push($errors, "Email is required."); }
-  if (empty($phone)) { array_push($errors, "Phone is required."); }
-  if (empty($user)) { array_push($errors, "Username is required."); }
-  if (empty($pw)) { array_push($errors, "Password is required."); }
-  if (empty($pw2)) { array_push($errors, "Please enter password again."); }
-  else if ($pw != $pw2) { array_push($errors, "Passwords do not match."); }
-
-  $sql = "SELECT * from users where username = '$user'";
-  $result = $conn->query($sql);
-  $num = mysqli_num_rows($result);
-
-  if ($num > 0) {
-    array_push($errors, "Username already exists. Please create a new username.");
-  }
-
-  if (count($errors) == 0) {
-
-    $sql = "INSERT INTO users (full_name, position, email, phone, username, password) VALUES('$fullname', '$position', '$email', '$phone', '$user', '$pw')";
-    $result = $conn->query($sql);
-
-    $sql = "CREATE USER '$user'@'localhost' IDENTIFIED BY '$pw'";
-    $result = $conn->query($sql);
-
-    header("location: login");
-  }
-}
+// connect to the database
 
 // Login user
-
 if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($conn, $_POST['username']);
   $password = mysqli_real_escape_string($conn, $_POST['password']);
@@ -69,7 +28,7 @@ if (isset($_POST['login_user'])) {
   }
 
   if (count($errors) == 0) {
-  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$query = "SELECT * FROM account WHERE username='$username' AND password='$password'";
   	$results = mysqli_query($conn, $query);
 
   	if (mysqli_num_rows($results) == 1) {
@@ -81,5 +40,9 @@ if (isset($_POST['login_user'])) {
   	}
   }
 }
+//Login User
+
+//Close connection
 CloseCon($conn);
+//Close connection
 ?>
